5987a254d664a94609cbbdd6d87f622b7bcd1948
Marta Hebda <marta.hebda@kcdo.pl>
2026-05-16 14:03:16 +0200

    All excercises

---
+++ terraform-s3-lab/variables.tf
@@ -0,0 +1,13 @@
+variable "regions" {
+  type = list(string)
+
+  default = [
+    "us-east-1",
+    "us-west-2"
+  ]
+}
+
+variable "bucket_name_prefix" {
+  type    = string
+  default = "marta-terraform-bucket"
+}
